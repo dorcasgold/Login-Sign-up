dd45117ef51aa7677ad3a36b83a1de36650a9d86
odetayo dorcas <odetayodorcas123@gmail.com>
2024-03-04 16:34:31 +0100

    create folder structure

---
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,16 @@
+import './LoginSignUp.css'
+import user-icon from '../Assets/person.png'
+import email-icon from '../Assets/email.png'
+import password-icon from '../Assets/password.png'
+
+const LoginSignUp = () => {
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">Sign Up</div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignUp
