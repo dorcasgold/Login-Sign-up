671440dc337e25bc8041180b4e2b60044ee82e12
odetayo dorcas <odetayodorcas123@gmail.com>
2024-03-04 16:46:09 +0100

    create markup

--- src/Components/LoginSignUp/LoginSignUp.jsx
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -1,13 +1,34 @@
 import './LoginSignUp.css'
-import user-icon from '../Assets/person.png'
-import email-icon from '../Assets/email.png'
-import password-icon from '../Assets/password.png'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
 
 const LoginSignUp = () => {
   return (
     <div className='container'>
       <div className="header">
         <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="user" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="email" />
+          <input type="email" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="password" />
+          <input type="password" />
+        </div>
+      </div>
+      <div className="forget-password">Lost Password? <span>Click Here</span></div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Log Up</div>
       </div>
     </div>
   )
